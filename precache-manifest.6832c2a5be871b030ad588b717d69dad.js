f210f9ef99df3fa9cb74562cef2c679741dbff97
shaishavkumar <shaishavkumar@users.noreply.github.com>
2020-09-23 12:51:57 +0530

    Updates

---
+++ precache-manifest.6832c2a5be871b030ad588b717d69dad.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccdbd8705d7d0b9d11f127606cb5224",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "adcf9d42d36dbc9b959a",
+    "url": "/home/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "3782558d4e63a5491b07",
+    "url": "/home/static/css/main.210661bd.chunk.css"
+  },
+  {
+    "revision": "adcf9d42d36dbc9b959a",
+    "url": "/home/static/js/2.02e3c56d.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/home/static/js/2.02e3c56d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3782558d4e63a5491b07",
+    "url": "/home/static/js/main.997f1392.chunk.js"
+  },
+  {
+    "revision": "8b5590f3a066af7b357e",
+    "url": "/home/static/js/runtime-main.256afb31.js"
+  },
+  {
+    "revision": "cf19a33c6787f27547daa0fbe93edcbe",
+    "url": "/home/static/media/About.cf19a33c.png"
+  },
+  {
+    "revision": "71e3e6b92e48faebfeed57c6bd5ad7b6",
+    "url": "/home/static/media/Android.71e3e6b9.jpg"
+  },
+  {
+    "revision": "92a758e9b9298b9ed692024d33db5417",
+    "url": "/home/static/media/App.92a758e9.jpg"
+  },
+  {
+    "revision": "9f2bed320fed92c8aab2ab69d16948ea",
+    "url": "/home/static/media/Digital.9f2bed32.jpg"
+  },
+  {
+    "revision": "911b46a3ea4de15d3922506ffdfb2c5f",
+    "url": "/home/static/media/Home.911b46a3.png"
+  },
+  {
+    "revision": "c740b91314bdcef1fb72f31cd58fcc21",
+    "url": "/home/static/media/Marketing.c740b913.jpg"
+  },
+  {
+    "revision": "e982025a2d29e4aa4e27f56dd6c67add",
+    "url": "/home/static/media/Software.e982025a.jpg"
+  },
+  {
+    "revision": "8bb024ba1e3ba185d8362e1c8bdfd42d",
+    "url": "/home/static/media/image1.8bb024ba.jpg"
+  },
+  {
+    "revision": "64ccccb82fd8ce68226dd8b360ef723d",
+    "url": "/home/static/media/logo.64ccccb8.png"
+  }
+]);
